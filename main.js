803716bbe88e63fadff4ea4a0f43bf11b073436a
Alina Pykus <alinapykus17@email.com>
2022-08-02 18:29:39 +0300

    add some changes

---
+++ main.js
@@ -0,0 +1,121 @@
+// Task 1
+let x = 1;
+let y = 2;
+
+
+let res1 = "" + x + y;
+//console.log(res1); // "12"
+ //console.log(typeof res1); // "string"
+
+ let res2 = String(!x + y);
+ //console.log(res2); // "true2"
+ //console.log(typeof res2); // "string"
+
+ let res3 = (x, y) ? true 
+ : false;
+ //console.log(res3); // true
+ //console.log(typeof res3); // "boolean"
+
+//  let res4 = parseInt('x', 'y');
+//  console.log(res4); // NaN
+//  console.log(typeof res4); // "number"
+
+
+
+// Task 2
+// let userNumb = prompt('write a number');
+// if (userNumb % 2 == 0) {
+//     console.log(true);
+// } else (userNumb % !2 == 0)
+//     console.log(false);
+
+
+// if (userNumb % 7 == 0) {
+//     console.log(true);
+//  } else (userNumb % !7 == 0)
+//      console.log(false);
+ 
+
+
+//Task 3
+//  const arr = [1, 'Hello', true, null]
+//  alert(arr.length);
+//   let user = prompt('write something');
+//   if (user.length > 1) {
+//     arr[5] = user;
+//   } else {
+//     console.log('something goes wrong');
+//   }
+//  arr.shift(1);
+//  alert(arr);
+
+
+
+// Task 4
+// const  cities = ["Rome", "Lviv", "Warsaw"]; 
+// console.log(cities.join('*'));
+
+
+// Task 5
+// let age = prompt('How old are you?');
+// let isAdult = age > 18;
+
+// if(isAdult > 18) {
+//     alert('you are adult');
+// } else (isAdult < 18)
+//     alert('you are not adult');
+// 
+
+
+
+// Task 6
+//  const A = prompt('write the lenght');
+//  const B = prompt('write the lenght');
+//  const C = prompt('write the lenght');
+
+//  const S = parseInt(1.2 * (A * B));
+//  console.log(S);
+
+//   if (S == 38) {
+//       console.log('right');
+//   } else ( S == !38)
+//   console.log('something goes wrong');
+
+
+
+// Task 7
+//  const currentDate = new Date();
+//  console.log(currentDate);
+
+//  if (currentDate < 23) {
+//      alert('Good night');
+//  } else if (currentDate < 5) {
+//      alert('Good mornig');
+//  } else if (currentDate < 11) {
+//      alert('Good day');
+//  } else (currentDate < 11) 
+//      alert('Good evening');
+ 
+    
+// make the rest of methods
+
+//const arr = ['Lviv', 'Germany', 'Poland', 'France', 'Italy'];
+
+//console.log(arr.slice(1, 2));
+
+//arr.splice(1, 0, 'Spanish');
+//console.log(arr);
+
+ //const arr2 = ['Mon', 'Tues', 'Wed'];
+// const arr3 = arr.concat(arr2);
+// console.log(arr3);
+
+//arr.forEach(el => console.log(el));
+
+// const num = [1, 2, 3];
+// const map = num.map(func);
+
+// function func(num) {
+// return num * 2;
+// }
+// console.log(map);
